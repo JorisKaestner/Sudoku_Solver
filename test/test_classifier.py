f7341ede200905a88b0315e6dc6200d6108964e0
Joris Kästner <joris-kaestner@web.de>
2026-08-14 18:19:00 +0200

    softened test cases to threshold evaluation

--- test/test_classifier.py
+++ test/test_classifier.py
@@ -2,11 +2,10 @@
 import pytest
 from sudoku_reader.grid_detector import load_image
 from sudoku_reader.grid_detector import extract_squares
-from sudoku_reader.grid_detector import resize_cells
 from sudoku_reader.cell_reader import classify_cell
 
-solutions = [
-[8,0,0,0,3,2,0,0,0,
+sudokus = {
+"sudoku_1": [8,0,0,0,3,2,0,0,0,
 0,0,0,0,1,6,7,8,9,
 1,4,6,0,0,0,0,0,2,
 3,8,2,0,0,0,0,9,0,
@@ -16,6 +15,7 @@ solutions = [
 0,5,0,0,0,9,6,3,1,
 0,3,0,8,7,0,0,0,0],
 
+"sudoku_2":
 [0,9,3,4,2,7,5,0,0,
 0,0,7,0,1,5,3,0,0,
 0,2,4,6,8,0,0,0,7,
@@ -26,6 +26,7 @@ solutions = [
 0,0,1,2,0,6,0,7,3,
 0,7,0,0,0,9,8,0,0],
 
+"sudoku_3":
 [0,3,0,0,0,0,0,0,0,
 0,0,0,1,9,5,0,0,0,
 0,0,8,0,0,0,0,6,0,
@@ -36,6 +37,7 @@ solutions = [
 0,0,0,4,1,9,0,0,5,
 0,0,0,0,0,0,0,7,0],
 
+"sudoku_4":
 [7,1,0,8,2,0,5,0,3,
 0,0,0,1,5,4,8,2,7,
 0,5,0,0,7,9,0,4,6,
@@ -46,6 +48,7 @@ solutions = [
 0,0,0,6,0,0,0,5,0,
 5,0,0,7,0,3,2,6,4],
 
+"sudoku_5":
 [0,0,0,0,9,5,3,0,0,
 5,0,0,0,0,0,6,2,0,
 3,0,4,0,0,0,0,0,0,
@@ -54,22 +57,25 @@ solutions = [
 4,6,0,0,0,0,0,0,7,
 0,0,0,0,8,7,0,0,6,
 7,0,0,0,0,0,9,0,1,
-0,5,1,0,0,0,0,0,0]]
+0,5,1,0,0,0,0,0,0]
+}
 
 def test_check_empty():
     img = load_image("test/sudoku_screenshots/sudoku_0.png")
-    empty_cells = resize_cells(extract_squares(img))
+    empty_cells = extract_squares(img)
     predictions = []
     for cell in empty_cells:
         predictions.append(classify_cell(cell))
     assert any(predictions) == False    # any() returns True for any value other than 0
 
-def test_full_grids():
-    for i in range(1,6):
-        img = load_image(f"test/sudoku_screenshots/sudoku_{i}.png")
-        cells = resize_cells(extract_squares(img))
-        predictions = []
-        for cell in cells:
-            predictions.append(classify_cell(cell))
-        assert predictions == solutions[i-1]
-
+@pytest.mark.parametrize("sudoku_name, expected", sudokus.items(), ids=sudokus.keys())
+def test_full_grid_accuracy(sudoku_name, expected):
+    img = load_image(f"test/sudoku_screenshots/{sudoku_name}.png")
+    cells = extract_squares(img)
+    predictions = [classify_cell(cell) for cell in cells]
+    count_correct = 0
+    for i in range(len(predictions)):
+        if (predictions[i] == expected[i]):
+            count_correct = count_correct + 1
+    accuracy = count_correct / 81
+    assert accuracy >= 0.95, f"{sudoku_name}: {accuracy:.1%} accuracy ({count_correct}/81)"
--- test/test_grid_detector.py
+++ test/test_grid_detector.py
@@ -6,7 +6,6 @@ from sudoku_reader.grid_detector import detect_edges
 from sudoku_reader.grid_detector import detect_lines
 from sudoku_reader.grid_detector import merge_nearest_lines
 from sudoku_reader.grid_detector import extract_squares
-from sudoku_reader.grid_detector import resize_cells
 
 SUDOKU_DIR = Path("test/sudoku_screenshots")
 SUDOKU_PATHS = sorted(SUDOKU_DIR.glob("sudoku_*"))
@@ -22,10 +21,3 @@ def test_count_detected_lines(path):
     sudoku = load_image(str(path))
     hor_lines, ver_lines = merge_nearest_lines(detect_lines(detect_edges(sudoku)))
     assert len(hor_lines) == 10 and len(ver_lines) == 10 # count top and bottom too
-
-@pytest.mark.parametrize("path", SUDOKU_PATHS, ids=lambda p: p.stem)
-def test_resize_cells(path):
-    sudoku = load_image(str(path))
-    cells = resize_cells(extract_squares(sudoku))
-    for cell in cells:
-        assert cell.shape == (28, 28)
